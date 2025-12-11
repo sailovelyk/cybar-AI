fc7615feb8bbc3dd33ae0b964fdc839675be6e90
sailovelyk <gurunadh299@gmail.com>
2025-12-11 14:21:58 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'encrypted-tbn0.gstatic.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+        ],
+    },
+};
+
+export default nextConfig;
